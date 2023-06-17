55bc0a92f477adf06203c820db79c79f794e3c4b
Juan Aguilera <j.aguilerahuerta@ugto.mx>
2023-06-17 15:30:45 -0600

    Adding variables file

---
+++ 01_variables.py
@@ -0,0 +1,18 @@
+#   Variables
+
+my_int_var = 14
+conv_string = str(my_int_var)  # Convirtiendo un entero a string
+print(conv_string)
+
+name = 'Juan Aguilera'
+age = 21
+alias = 'Juan Gdev'
+
+print("Me llamo: ", name, ".\nMi edad es: ", age, "\nMi alias es: ", alias)
+
+#   Inputs
+first_name = input("What's your first name?\n")
+age = input("How old are you?\n")
+
+print(first_name, "\n", age)
+
