3e1946078aa9ece1d5d25d62d3a9ff3f9db1fb4f
Juan Aguilera <j.aguilerahuerta@ugto.mx>
2023-06-17 14:34:05 -0600

    Renaming file and type

---
+++ 00_hello.py
@@ -0,0 +1,13 @@
+print("Hello world!")
+"""
+Long Commentary
+.
+"""
+
+# Consultar el tipo de dato
+print(type("Soy un string"))
+print(type(5))      # Tipo int
+print(type(1.5))    # Tipo float
+print(type(1+3j))   # Tipo complex
+print(type(True))   # Tipo bool
+
--- hello.py
+++
@@ -1,9 +0,0 @@
-print("Hello world!")
-"""
-Comentario en varias líneas
-jijiji
-"""
-
-# Consultar el tipo de dato
-print(type("Hello World"))
-
