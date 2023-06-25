2adfbb0f85d987a301373b4163f2b51db0b9b738
Juan Aguilera <j.aguilerahuerta@ugto.mx>
2023-06-24 22:02:56 -0600

    Operators basics

---
+++ 02_operadores.py
@@ -0,0 +1,17 @@
+##  Operadores  ##
+
+#   Floor Division Operator -> "//"
+#   This result will always be the lowest integer
+print(10 // 3)
+
+#   Exponent
+print(2 ** 4)
+
+print(3 >= 2)
+
+print("aaaa" >= "aaaa") ##  Ordenamiento alfabetico
+
+#   Logic Operators
+print(3 < 4 and 5 < 12 )    #   en python no se usa &&, ||
+
+
